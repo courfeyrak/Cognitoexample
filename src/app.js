45f08e8f27bcea5da472d9a33e8eeb72000451ea
Carlos <carlos.gutierrez@xherstone.com>
2021-10-15 21:26:07 -0500

    se agrego el login manual con usurio de cognito

--- src/app.js
+++ src/app.js
@@ -1,3 +1,52 @@
 import Amplify, { Auth } from 'aws-amplify';
 import awsconfig from './aws-exports';
 Amplify.configure(awsconfig);
+
+
+async function signIn() {
+    try {
+        var username="carlos";
+        var password="Hgijrt89$";
+        const user = await Auth.signIn(username, password);
+        console.log(user);
+    } catch (error) {
+        console.log('error signing in', error);
+    }
+}
+
+async function signUp() {
+    try {
+        var username="carlos";
+        var password="Hgijrt89$";
+        var email="carlos.gtz.cg@gmail.com";
+        var phone_number="";
+        const { user } = await Auth.signUp({
+            username,
+            password,
+            attributes: {
+                email,          // optional
+                phone_number,   // optional - E.164 number convention
+                // other custom attributes 
+            }
+        });
+        console.log(user);
+    } catch (error) {
+        console.log('error signing up:', error);
+    }
+}
+
+const MutationButton = document.getElementById("MutationEventButton");
+const adduserButton = document.getElementById("addUserEventButton");
+
+
+MutationButton.addEventListener("click", (evt) => {
+    signIn().then((evt) => {
+      MutationResult.innerHTML += `<p>${evt.data.createTodo.name} - ${evt.data.createTodo.description}</p>`;
+    });
+  });
+
+  adduserButton.addEventListener("click", (evt) => {
+    signUp().then((evt) => {
+      MutationResult.innerHTML += `<p>${evt.data.createTodo.name} - ${evt.data.createTodo.description}</p>`;
+    });
+  });
